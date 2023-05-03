5a59fccc8cc1054691482d20293a6fe0d870b256
Felipe Lima <felipe.lima@FLEX.LOCAL>
2023-05-03 14:23:55 -0300

    Configuração do projeto

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,11 @@
+import Styles from "./Cardapio.module.scss"
+
+export default function Cardapio() {
+  return (
+    <main>
+        <nav className={Styles.menu}>
+            Aluroni
+        </nav>
+    </main>
+  )
+}
