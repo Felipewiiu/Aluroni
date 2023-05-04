791ae8576fbbd765b3ef5d80bf6f861c388c4565
Felipe Lima <felipe.lima@FLEX.LOCAL>
2023-05-04 14:44:15 -0300

    Colocando o svg

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -4,7 +4,7 @@ export default function Cardapio() {
   return (
     <main>
         <nav className={Styles.menu}>
-            Aluroni
+            <img src="../../assets.logo.svg" alt="Logo do aluroni" />
         </nav>
     </main>
   )
