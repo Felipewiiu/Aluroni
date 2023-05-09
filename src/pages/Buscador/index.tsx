33be2ff994d3af4c0d6329dee4e526c4fcec9d91
Felipe Lima <felipe.lima@FLEX.LOCAL>
2023-05-09 14:13:38 -0300

    Criando o componente Buscador

---
+++ src/pages/Buscador/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import styles from "./Buscador.module.scss"
+
+interface Props{
+    busca: string,
+    setBusca: React.Dispatch<React.SetStateAction<string>>;
+    //<React.SetStateAction<string>>; --> Dispatch diz que vai disparar algo
+}
+
+export default function Buscador({busca, setBusca}: Props) {
+  return (
+    <div>Buscador</div>
+  )
+}
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,13 +1,31 @@
+
 import Styles from "./Cardapio.module.scss"
-import { ReactComponent as Logo } from "assets/logo.svg";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
+import Buscador from "pages/Buscador";
+import { useState } from "react"
 
 export default function Cardapio() {
+  const [busca, setBusca] = useState("")
+
   return (
     <main>
         <nav className={Styles.menu}>
             <Logo/>
             Aluroni
         </nav>
+        <header className={Styles.header}>
+          <div className={Styles.header__text}>
+              A casa do código e da massa
+          </div>
+        </header>
+        <section className={Styles.cardapio}>
+          <h3 className={Styles.cadapio__titulo}>Cardápio</h3>
+          <Buscador 
+            busca={busca} 
+            setBusca={setBusca}
+          />
+        </section>
+        
     </main>
   )
 }
