90c4375ca7d72fac27d54cfb276dc4ab3468bff4
Felipe Santa Cruz <91490113+Felipewiiu@users.noreply.github.com>
2023-06-13 21:55:19 -0300

    Criando a rita notFound

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import cardapio from 'data/cardapio.json';
 import styles from './inicio.module.scss';
 import stylesTema from 'styles/Tema.module.scss';
+import nossaCasa from 'assets/nossa_casa.png';
 
 export default function Inicio() {
   let pratosRecomendados = [...cardapio];
@@ -23,6 +24,13 @@ export default function Inicio() {
             </button>
           </div>
         ))}
+      </div>
+      <h3 className={stylesTema.titulo}>Nossa casa</h3>
+      <div className={styles.nossaCasa}>
+        <img src={nossaCasa} alt="Casa do aluroni" />
+        <div className={styles.nossaCasa__endereco}>
+            Rua vergueiro 3185 <br /> <br /> Vila Mariana - SP
+        </div>
       </div>
     </section>
   );
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
+import Footer from 'Components/Footer';
 import Menu from 'Components/Menu';
 import PaginaPadrao from 'Components/PaginaPadrao';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
+import NotFound from 'pages/NotFound';
+import Sobre from 'pages/Sobre';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function AppRouter() {
@@ -10,13 +13,15 @@ export default function AppRouter() {
       <Router>
         <Menu />
         <Routes>
-          <Route path='/' element={<PaginaPadrao/>}>
-            <Route index element={<Inicio/>}/>
+          <Route path='/' element={<PaginaPadrao />}>
+            <Route index element={<Inicio />} />
             <Route path='cardapio' element={<Cardapio />} />
+            <Route path='sobre' element={<Sobre />} />
+            <Route path='*' element={<NotFound />} />
           </Route>
-          <Route path='sobre' element={<div><h1>Apenas um teste</h1></div>}/>
-          
+
         </Routes>
+        <Footer />
       </Router>
     </main>
   );
